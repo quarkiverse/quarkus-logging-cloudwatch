be31cf46f1a15d6a0dbff013b98e6aa1148b9645
Bennet Schulz <mail@bennet-schulz.de>
2021-06-17 08:53:54 +0200

    Add test for Sequence Token

---
+++ runtime/src/test/java/io/quarkiverse/logging/cloudwatch/LoggingCloudWatchHandlerTest.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.logging.cloudwatch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoggingCloudWatchHandlerTest {
+
+    private LoggingCloudWatchHandler handler;
+
+    LoggingCloudWatchHandlerTest() {
+        this.handler = new LoggingCloudWatchHandler();
+    }
+
+    @Test
+    void shouldExtractNextSequenceToken() {
+        Exception e = new RuntimeException("The given sequenceToken is invalid. The next expected sequenceToken is: 49611889230645343415657219696041834137426496333583221330 (Service: AWSLogs; Status Code: 400; Error Code: InvalidSequenceTokenException; Request ID: 02120a3d-bc00-45df-96fd-b3491ba01924; Proxy: null)");
+        String exceptionMessage = e.getMessage();
+        final String actual = handler.extractValidSequenceToken(exceptionMessage);
+
+        assertEquals("49611889230645343415657219696041834137426496333583221330", actual);
+    }
+}
