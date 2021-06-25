98c9c2e7403b187442204d1b5189290faf16919b
Gwenneg Lepage <gwenneg@gmail.com>
2021-06-25 09:53:23 +0200

    Lazily recompute EqualJitterBackoffStrategy#Random at runtime

---
+++ runtime/src/main/java/io/quarkiverse/logging/cloudwatch/graal/AwsSubstitutions.java
@@ -0,0 +1,38 @@
+package io.quarkiverse.logging.cloudwatch.graal;
+
+import java.util.Random;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.InjectAccessors;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(className = "com.amazonaws.retry.PredefinedBackoffStrategies$EqualJitterBackoffStrategy")
+final class AwsSubstitutions {
+
+    @Alias
+    @InjectAccessors(RandomAccessors.class)
+    private static Random random;
+
+    public static final class RandomAccessors {
+
+        private static volatile Random volatileRandom;
+
+        public static Random get() {
+            Random localVolatileRandom = volatileRandom;
+            if (localVolatileRandom == null) {
+                synchronized (RandomAccessors.class) {
+                    localVolatileRandom = volatileRandom;
+                    if (localVolatileRandom == null) {
+                        volatileRandom = localVolatileRandom = new Random();
+                    }
+                }
+            }
+            return localVolatileRandom;
+        }
+
+        public static void set(Random random) {
+            throw new IllegalStateException(
+                    "The setter for com.amazonaws.retry.PredefinedBackoffStrategies$EqualJitterBackoffStrategy#random shouldn't be called.");
+        }
+    }
+}
