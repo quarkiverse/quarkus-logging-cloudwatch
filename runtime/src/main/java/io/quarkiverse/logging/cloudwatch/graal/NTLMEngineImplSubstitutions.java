7d8dd2c8c6ea975e452bdf2790741b586585c333
Bennet Schulz <mail@bennet-schulz.de>
2021-07-07 09:11:33 +0200

    Fix native image build

---
+++ runtime/src/main/java/io/quarkiverse/logging/cloudwatch/graal/NTLMEngineImplSubstitutions.java
@@ -0,0 +1,38 @@
+package io.quarkiverse.logging.cloudwatch.graal;
+
+import java.security.SecureRandom;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.InjectAccessors;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(className = "org.apache.http.impl.auth.NTLMEngineImpl")
+final class NTLMEngineImplSubstitutions {
+
+    @Alias
+    @InjectAccessors(RandomAccessors.class)
+    private static SecureRandom RND_GEN;
+
+    public static final class RandomAccessors {
+
+        private static volatile SecureRandom volatileRandom;
+
+        public static SecureRandom get() {
+            SecureRandom localVolatileRandom = volatileRandom;
+            if (localVolatileRandom == null) {
+                synchronized (RandomAccessors.class) {
+                    localVolatileRandom = volatileRandom;
+                    if (localVolatileRandom == null) {
+                        volatileRandom = localVolatileRandom = new SecureRandom();
+                    }
+                }
+            }
+            return localVolatileRandom;
+        }
+
+        public static void set(Object object) {
+            throw new IllegalStateException(
+                    "The setter for org.apache.http.impl.auth.NTLMEngineImpl#random shouldn't be called.");
+        }
+    }
+}
