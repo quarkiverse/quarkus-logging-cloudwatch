b2f27332b502e53f33940e78cbcb5aa9fa7460d9
Bennet Schulz <mail@bennet-schulz.de>
2021-07-12 07:35:30 +0200

    Remove integration tests

--- integration-tests/src/test/java/io/quarkiverse/logging/cloudwatch/it/NativeLoggingCloudwatchHandlerResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.logging.cloudwatch.it;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeLoggingCloudwatchHandlerResourceIT extends LoggingCloudwatchHandlerResourceTest {
-}
