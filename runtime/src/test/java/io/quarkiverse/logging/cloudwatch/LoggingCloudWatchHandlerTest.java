f7c3432fa5aae066f6e2b3392bfeaf7f55bc28bb
Bennet Schulz <mail@bennet-schulz.de>
2022-02-04 10:57:12 +0100

    Remove obsolete constructor

--- runtime/src/test/java/io/quarkiverse/logging/cloudwatch/LoggingCloudWatchHandlerTest.java
+++ runtime/src/test/java/io/quarkiverse/logging/cloudwatch/LoggingCloudWatchHandlerTest.java
@@ -9,18 +9,14 @@ import org.junit.jupiter.api.Test;
 
 class LoggingCloudWatchHandlerTest {
 
-    private final LoggingCloudWatchHandler handler;
-
-    LoggingCloudWatchHandlerTest() {
-        this.handler = new LoggingCloudWatchHandler();
-    }
+    private final LoggingCloudWatchHandler testee = new LoggingCloudWatchHandler();
 
     @Test
     void shouldExtractNextSequenceToken() {
         Exception e = new RuntimeException(
                 "The given sequenceToken is invalid. The next expected sequenceToken is: 49611889230645343415657219696041834137426496333583221330 (Service: AWSLogs; Status Code: 400; Error Code: InvalidSequenceTokenException; Request ID: 02120a3d-bc00-45df-96fd-b3491ba01924; Proxy: null)");
         String exceptionMessage = e.getMessage();
-        final String actual = handler.extractValidSequenceToken(exceptionMessage);
+        final String actual = testee.extractValidSequenceToken(exceptionMessage);
 
         assertEquals("49611889230645343415657219696041834137426496333583221330", actual);
     }
@@ -30,7 +26,7 @@ class LoggingCloudWatchHandlerTest {
         Exception e = new RuntimeException(
                 "The given sequenceToken is invalid. The next expected sequenceToken is:  49611889230645343415657219696041834137426496333583221330  (Service: AWSLogs; Status Code: 400; Error Code: InvalidSequenceTokenException; Request ID: 02120a3d-bc00-45df-96fd-b3491ba01924; Proxy: null)");
         String exceptionMessage = e.getMessage();
-        final String actual = handler.extractValidSequenceToken(exceptionMessage);
+        final String actual = testee.extractValidSequenceToken(exceptionMessage);
 
         assertEquals("49611889230645343415657219696041834137426496333583221330", actual);
     }
@@ -38,35 +34,35 @@ class LoggingCloudWatchHandlerTest {
     @Test
     void shouldBeBelowThresholdWhenBothAreInfo() {
         LogRecord record = new LogRecord(Level.INFO, "someMessage");
-        handler.setLevel(Level.INFO);
-        assertFalse(handler.isBelowThreshold(record));
+        testee.setLevel(Level.INFO);
+        assertFalse(testee.isBelowThreshold(record));
     }
 
     @Test
     void shouldBeBelowThresholdWhenLogRecordIsWarnAndHandlerLevelIsInfo() {
         LogRecord record = new LogRecord(Level.WARN, "someMessage");
-        handler.setLevel(Level.INFO);
-        assertFalse(handler.isBelowThreshold(record));
+        testee.setLevel(Level.INFO);
+        assertFalse(testee.isBelowThreshold(record));
     }
 
     @Test
     void shouldBeBelowThresholdWhenLogRecordIsInfoAndHandlerLevelIsWarn() {
         LogRecord record = new LogRecord(Level.INFO, "someMessage");
-        handler.setLevel(Level.WARN);
-        assertTrue(handler.isBelowThreshold(record));
+        testee.setLevel(Level.WARN);
+        assertTrue(testee.isBelowThreshold(record));
     }
 
     @Test
     void shouldBeAboveThresholdWhenLogRecordIsWarnAndHandlerLevelIsSevere() {
         LogRecord record = new LogRecord(Level.WARN, "someMessage");
-        handler.setLevel(Level.SEVERE);
-        assertTrue(handler.isBelowThreshold(record));
+        testee.setLevel(Level.SEVERE);
+        assertTrue(testee.isBelowThreshold(record));
     }
 
     @Test
     void shouldBeAboveThresholdWhenLogRecordIsInfoAndHandlerLevelIsSevere() {
         LogRecord record = new LogRecord(Level.INFO, "someMessage");
-        handler.setLevel(Level.SEVERE);
-        assertTrue(handler.isBelowThreshold(record));
+        testee.setLevel(Level.SEVERE);
+        assertTrue(testee.isBelowThreshold(record));
     }
 }
